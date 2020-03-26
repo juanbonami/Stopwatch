acf69cde241494d722e565620eb29b7940627910
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-25 23:24:47 -0500

    pseudocode

---
+++ src/pseudocode.js
@@ -0,0 +1,21 @@
+// create an opening <div> tag and a closing </div> tag
+// give it an id of parent container id="parent"
+// inbetween the div tags
+// create opening and closing <h1> tags
+// and use this to display the timer.
+// begin with <h1> 0 </h1>
+// create another <div> 
+// giv this <div> an id of btns
+// id="Btns"
+// inside this <div>..
+// create 3 buttons 
+// name your buttons
+// <button>Start</button>
+// <button>Pause</button>
+// <button>Reset</button>
+// give buttons functionality by creating functions for each individual button
+// refrence those functions by calling the onclick="function()"
+// create functionality for the <h1>
+// the timer should start at zero and incrementally go up 
+// we can create functionality for it and render it by using the .innerHTML
+// and we can stop, start, or reset depending on which buttons are clicked by the user.
