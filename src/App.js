fd1a97ff179f0b43070e708fc2ffeb49257175cd
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-28 21:39:00 -0500

    startPressed function

--- src/App.js
+++ src/App.js
@@ -11,16 +11,61 @@ export default class App extends Component {
   }
 
 
+//   clicks = () => {
+//     setInterval(()=> {
+//       this.state.counter = this.state.counter + 1
+//     }, 1000)
+// }
+
+// componentDidMount() {
+//     this.myInterval = setInterval(()=> {
+//         this.setState({
+//               counter: this.state.counter +1
+//             })
+//     }, 1000)
+    
+//   }
+
+
+  startPressed = () => {
+    this.setState({
+      counter: current += setInterval(()=>{
+        this.setState({
+          counter: this.state.counter + 1
+        })
+      }, 1000)
+    })
+  }
+
+  // startPressed = () => {
+  //   this.setState({
+  //     counter: setInterval(this.state.counter = this.state.counter + 1)
+  //   })
+  // }
+
+  
+
+  
+
+  pausePressed = () => {
+    this.setState({
+      counter: 4
+    })
+  }
+
+  
+
+
 
   render() {
+    let current = 0;
     return (
-      <div>
+      <div className="App">
         <h1>{this.state.counter}</h1>
         <button className="reset">Reset</button>
-        <button className="start">Start</button>
+        <button className="start" onClick={this.startPressed}>Start</button>
         <button className="pause">Pause</button>
       </div>
     )
   }
 }
-
